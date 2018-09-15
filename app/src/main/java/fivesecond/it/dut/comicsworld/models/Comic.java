6378304f2530d26787a022447c35f53092bd8d9e
sirenas23298 <sirenas23298@gmail.com>
2018-09-15 03:03:33 -0700

    Commit controllers and models

---
+++ app/src/main/java/fivesecond/it/dut/comicsworld/models/Comic.java
@@ -0,0 +1,16 @@
+package fivesecond.it.dut.comicsworld.models;
+
+public class Comic {
+    private int id;
+    private String name;
+    private int idType;
+    private String thumbnail;
+    private int chaps;
+    private String description;
+    private int view;
+    private float rated;
+    private int idComment;
+    private String author;
+
+
+}
