e8e498f51449c0f8e0f6e6f793bc164cf5f339ff
sonvofriend <sonvofriend@gmail.com>
2018-09-21 18:23:06 +0700

    Update list comics

---
+++ app/src/main/java/fivesecond/it/dut/comicsworld/ListComicsActivity.java
@@ -0,0 +1,13 @@
+package fivesecond.it.dut.comicsworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListComicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_comics);
+    }
+}
