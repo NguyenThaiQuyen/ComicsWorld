b244153a12422e159f2487b585caf9127f45b713
sonvofriend <sonvofriend@gmail.com>
2018-11-06 18:21:54 +0700

    update

--- app/src/main/java/fivesecond/it/dut/comicsworld/controllers/ComicController.java
+++ app/src/main/java/fivesecond/it/dut/comicsworld/controllers/ComicController.java
@@ -42,19 +42,19 @@ public class ComicController implements Serializable{
 
     public void fakeData()
     {
-        mComicList.add(new Comic(1, "N11", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(2, "N12", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(3, "N13", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(4, "N14", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(5, "N21", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(6, "N22", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(7, "N23", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(8, "N14", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(9, "N31", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(10, "N32", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(11, "N33", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(12, "N24", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
-        mComicList.add(new Comic(13, "N34", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo"));
+        mComicList.add(new Comic(1, "N11", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo", ""));
+        mComicList.add(new Comic(2, "N12", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo", ""));
+        mComicList.add(new Comic(3, "N13", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(4, "N14", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(5, "N21", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(6, "N22", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(7, "N23", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(8, "N14", 1, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(9, "N31", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(10, "N32", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(11, "N33", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(12, "N24", 2, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
+        mComicList.add(new Comic(13, "N34", 3, "@drawable/conan.jpg", 5, "You are the Apple of my eye", 0, 4,5, "Sonvo",""));
 
     }
 
