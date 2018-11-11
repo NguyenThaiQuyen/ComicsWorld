e793b6239d55ac22b00b2a6392a2f8f4ce5aac8d
sonvofriend <sonvofriend@gmail.com>
2018-11-11 22:42:28 +0700

    update

--- app/src/main/java/fivesecond/it/dut/comicsworld/ListComicsActivity.java
+++ app/src/main/java/fivesecond/it/dut/comicsworld/ListComicsActivity.java
@@ -1,28 +1,16 @@
 package fivesecond.it.dut.comicsworld;
 
-<<<<<<< HEAD
-
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 import fivesecond.it.dut.comicsworld.adapters.ListViewContentAdapter;
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import fivesecond.it.dut.comicsworld.async.LoadComicCondition;
->>>>>>> 2852a0e9e2cd287349112e54d524d56dc81c46ad
 
 
 public class ListComicsActivity extends AppCompatActivity {
