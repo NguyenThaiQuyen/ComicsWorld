4baec6ff2a9873e20eec6d179e1b85f5ee7afaf8
Nguyen Thai Quyen <thai.quyen012@gmail.com>
2018-11-24 13:16:03 +0700

    update select language

--- app/src/main/java/fivesecond/it/dut/comicsworld/MainActivity.java
+++ app/src/main/java/fivesecond/it/dut/comicsworld/MainActivity.java
@@ -1,83 +1,132 @@
 package fivesecond.it.dut.comicsworld;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStart;
-    Button btnContinue;
+    Button btnStart, btnChange, btnContinue;
     private SharedPreferences sharedPreferences;
     private static final String myref = "currentComic";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sharedPreferences = getSharedPreferences(myref, Context.MODE_PRIVATE);
+
+        String language = sharedPreferences.getString("KEY_LANGUAGE", "en");
+        setLanguage(language);
+
         setContentView(R.layout.activity_main);
 
         inits();
-        setWidgets();
         getWidgets();
+        setWidgets();
         addListenners();
     }
 
     private void inits() {
-        sharedPreferences = getSharedPreferences(myref, Context.MODE_PRIVATE);
+
+    }
+
+
+    private void getWidgets() {
+        btnStart = findViewById(R.id.btnStart);
+        btnContinue = findViewById(R.id.btnContinue);
+        btnChange = findViewById(R.id.btnChange);
     }
 
     private void setWidgets() {
-        btnStart = findViewById(R.id.btnStart);
-        btnContinue = findViewById(R.id.btnContinue);
 
-    }
-
-    private void getWidgets() {
 
     }
 
 
     private void addListenners() {
-
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.clear().apply();
-
                 Intent intent = new Intent(MainActivity.this, HomeScreenActivity.class);
                 startActivity(intent);
             }
         });
 
-        if(sharedPreferences.contains("urlComic"))
-        {
-            btnContinue.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext() ,ReadComic.class);
-                    intent.putExtra("url", sharedPreferences.getString("urlComic", "1"));
-                    intent.putExtra("chap", sharedPreferences.getInt("chap", 1));
-                    intent.putExtra("totalChap", sharedPreferences.getInt("totalChap", 3));
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSelectLanguage();
+            }
+        });
+        if (sharedPreferences.contains("urlComic")) {
+            btnContinue.setVisibility(View.VISIBLE);
+           btnContinue.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent intent = new Intent(getApplicationContext(), ReadComic.class);
+                   intent.putExtra("url", sharedPreferences.getString("urlComic", "1"));
+                   intent.putExtra("chap", sharedPreferences.getInt("chap", 1));
+                   intent.putExtra("totalChap", sharedPreferences.getInt("totalChap", 3));
 
-                    startActivity(intent);
-                }
-            });
-        }
-        else
+                   startActivity(intent);
+               }
+           });
+        } else
         {
             btnContinue.setVisibility(View.GONE);
         }
     }
 
+    public void showSelectLanguage() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.title_dialog_main)
+                .setItems(R.array.languages, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String language = "en";
+                        switch (which) {
+                            case 1: //VI
+                                language = "vi";
+                                break;
+                        }
+                        setLanguage(language);
 
+                        //save
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("KEY_LANGUAGE", language);
+                        editor.apply();
 
+                        //refresh UI
+                        setContentView(R.layout.activity_main);
+                        inits();
+                        getWidgets();
+                        setWidgets();
+                        addListenners();
+                    }
+                }).create().show();
+    }
 
-
-
+    public void setLanguage(String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration con = resources.getConfiguration();
+        con.locale = locale;
+        resources.updateConfiguration(con, resources.getDisplayMetrics());
+    }
 }
+
